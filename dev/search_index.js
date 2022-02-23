3635f326a31b53ce7c1592fd8ac5a75c65ea9442
Documenter.jl <documenter@juliadocs.github.io>
2022-02-23 19:53:12 +0000

    build based on c04edee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorTutorials","category":"page"},{"location":"#ITensorTutorials","page":"Home","title":"ITensorTutorials","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorTutorials.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorTutorials]","category":"page"}]
+}
