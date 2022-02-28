148a34373495bb46db38901cb18f4bfc6cd66311
Matthew Fishman <mfishman@flatironinstitute.org>
2022-02-28 08:34:37 -0800

    Finishing formatting MPO slide. Fully compile tutorials

--- tutorials/package_compile/package_compile.jl
+++ tutorials/package_compile/package_compile.jl
@@ -2,7 +2,13 @@ using PackageCompiler
 
 tutorials_file = joinpath("..", "12_n_site.jl")
 PackageCompiler.create_sysimage(
-  ["ITensors", "ITensorUnicodePlots", "ITensorGLMakie"];
-  sysimage_path="ITensorTutorials.so",
+  ["ITensors", "ITensorUnicodePlots", "ITensorGLMakie", "Zygote"];
+  sysimage_path="ITensor.so",
   precompile_execution_file=tutorials_file,
 )
+
+# Then make an alias like:
+#
+# alias julia-itensor='julia -J/home/mfishman/.julia/dev/ITensorTutorials/tutorials/package_compile/ITensor.so'
+#
+# and run `julia-itensor`.
