3727f28041f0f50105221049d342354d26452422
Matthew Fishman <mfishman@flatironinstitute.org>
2022-03-30 21:01:00 -0400

    Fix mixup of Z+ and Z- in 2-qubit control gate example \(thanks to Miles Stoudenmire for catching the mistake!\)

--- tutorials/08_two_site_operator_custom.jl
+++ tutorials/08_two_site_operator_custom.jl
@@ -16,17 +16,16 @@ function op(::OpName"CRy", ::SiteType"S=1/2"; θ)
     1 0 0           0
     0 1 0           0
     0 0 cos(θ / 2) -sin(θ / 2)
-    0 0 sin(θ / 2)  cos(θ / 2)
+    0 0 sin(θ / 2) cos(θ / 2)
   ]
 end
 
-# Controlled-Hadamard gate, CH = CRy(θ=π/2)
-CH = op("CRy", i1, i2; θ=π/2)
+# CRy(θ=π/2)
+θ = π / 2
+CRyθ = op("CRy", i1, i2; θ)
 
-# |10⟩ = |Z+Z-⟩
-ZpZm = Zp1 * Zm2
+# |10⟩ = |Z-Z+⟩
+ZmZp = Zm1 * Zp2
 
-CH_Xm = apply(CH, ZpZm)
-
-# CH|Z+Z-⟩ = |Z+X-⟩
-@show CH_Xm ≈ Zp1 * Xm2
+# CRyθ|Z-Z+⟩ = |Z-X+⟩
+@show apply(CRyθ, ZmZp) ≈ Zm1 * Xp2
